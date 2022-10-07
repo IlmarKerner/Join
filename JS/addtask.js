721d7e140cc40b52c407d8f13983f80a780b2ea2
IlmarKerner <kernerilmar@gmail.com>
2022-10-07 21:57:49 +0200

    add tasks with js 3

--- JS/addtask.js
+++ JS/addtask.js
@@ -1,15 +1,17 @@
 let contactSectionOpen = false;
 let selectTaskBoxOpen = false;
 let urgentImage = false;
+let mediumImage = false;
+let lowImage = false;
 
 function showSelctedContacts() {
     let selectbox = document.getElementById('assigned');
     if (!contactSectionOpen) {
-        selectbox.classList.add('height200');
+        selectbox.classList.add('height210');
         selectbox.classList.add('transition');
         contactSectionOpen = true;
     } else {
-        selectbox.classList.remove('height200');
+        selectbox.classList.remove('height210');
         selectbox.classList.add('height51');
         contactSectionOpen = false;
     }
@@ -18,11 +20,11 @@ function showSelctedContacts() {
 function selectTaskCategory() {
     let selectTaskBox = document.getElementById('selectTask');
     if (!selectTaskBoxOpen) {
-        selectTaskBox.classList.add('height200');
+        selectTaskBox.classList.add('height360');
         selectTaskBox.classList.add('transition');
         selectTaskBoxOpen = true;
     } else {
-        selectTaskBox.classList.remove('height200');
+        selectTaskBox.classList.remove('height360');
         selectTaskBox.classList.add('height51');
         selectTaskBoxOpen = false;
     }
@@ -30,15 +32,36 @@ function selectTaskCategory() {
 
 function changeImgUrgent() {
     let urgent = document.getElementById('urgent');
-    urgent.scr = "../img/urgentbuttonwhite.png";
+    urgent.src = "../img/urgentbutton.png";
+    if (urgentImage) {
+        urgent.src = "../img/urgentbutton.png";
+        urgentImage = false;
+    } else {
+        urgent.src = "../img/urgentbuttonwhite.png";
+        urgentImage = true;
+    }
+}
 
-    // let urgent = document.getElementById('urgent');
-    // if (!urgentImage) {
-    //     urgent.style = "background: none;"
-    //     urgent.style = "background: "
-    //     urgentImage = false;
-    // } else {
-    //     urgent.src = "../img/urgentbuttonwhite.png"
-    //     urgentImage = true;
-    // }
+function changeImgMedium() {
+    let medium = document.getElementById('medium');
+    medium.src = "../img/mediumbutton.png";
+    if (mediumImage) {
+        medium.src = "../img/mediumbutton.png";
+        mediumImage = false;
+    } else {
+        medium.src = "../img/mediumbuttonwhite.png";
+        mediumImage = true;
+    }
+}
+
+function changeImgLow() {
+    let low = document.getElementById('low');
+    low.src = "../img/lowbutton.png";
+    if (lowImage) {
+        low.src = "../img/lowbutton.png";
+        lowImage = false;
+    } else {
+        low.src = "../img/lowbuttonwhite.png";
+        lowImage = true;
+    }
 }
