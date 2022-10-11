d69ce3d0b0a6c908f3693aa4158909c0f59906ad
lNeureiter <lukas_neureiter@gmx.at>
2022-10-11 21:57:27 +0200

    Drag and Drop

--- JS/board.js
+++ JS/board.js
@@ -1,19 +1,4 @@
-let currentDraggedElement;
-
-
-function startDragging(id){
-    currentDraggedElement = id;
-    
-}
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-
-function moveTo(category) {
-    
-}
+let currentDraggedItem = document.getElementById('testID');
 
 
 function addDropPosition() {
@@ -23,9 +8,29 @@ function addDropPosition() {
     document.getElementById('doneDropPosition').classList.remove('dNone');
 }
 
+
 function removeDropPosition() {
     document.getElementById('toDoDropPosition').classList.add('dNone');
     document.getElementById('inProgressDropPosition').classList.add('dNone');
     document.getElementById('awaitingFeedbackDropPosition').classList.add('dNone');
     document.getElementById('doneDropPosition').classList.add('dNone');
 }
+
+
+currentDraggedItem.addEventListener('dragstart', dragStart);
+currentDraggedItem.addEventListener('dragend', dragEnd);
+
+
+function dragStart() {
+    this.className += 'tilt';
+    setTimeout(() => (this.className = 'dNone'), 0);
+    console.log('start');
+}
+
+function dragEnd() {
+    console.log('end');
+}
+
+
+
+
