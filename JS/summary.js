28e73403b775b41ed16da0616039ee1233ea6265
IlmarKerner <kernerilmar@gmail.com>
2022-11-01 19:56:14 +0100

    summary und board JS

--- JS/summary.js
+++ JS/summary.js
@@ -1,3 +1,22 @@
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+let progressNumber = 0;
+let awaitingFeedback = 0;
+let toDo = 0;
+let tasksDone = 0;
+let urgent = 0;
+
+function fillSummaryInfos() {
+    for (i = 0; i < tasks.length; i++) {
+        if (tasks[i]['status'] == 'In progress') { tasksinProgress++; };
+        if (tasks[i]['status'] == 'Awaiting feedback') { tasksawaitingFeedback++; };
+        if (tasks[i]['priority'] == 'Urgent') { tasksUrgent++; if (transformDate(tasks[i]['dueDate']) > nextDeadlineofUrgentTasks) { nextDeadlineofUrgentTasks = transformDate(tasks[i]['dueDate']); }; };
+        if (tasks[i]['status'] == 'To do') { taskstoDo++; };
+        if (tasks[i]['status'] == 'Done') { tasksDone++; };
+    }
+}
+
 function showNumberOfTasks() {
     let boardNumber = document.getElementById('tasksInBoard');
     boardNumber.innerHTML = '';
@@ -6,32 +25,20 @@ function showNumberOfTasks() {
 
 function showNumberOfTasksInProgress() {
     let progressNumber = document.getElementById('tasksInProgress');
-    progressNumber.innerHTML = '';
-    for (let i = 0; i < tasks.length; i++) {
-        progressNumber.innerHTML = tasks[i]['progress'].length;
-    }
+    progressNumber.innerHTML = tasks['progress'];
 }
 
 function showNumberOfTasksAwaitingFeedback() {
     let progressNumber = document.getElementById('awaitingFeedback');
     progressNumber.innerHTML = '';
-    for (let i = 0; i < tasks.length; i++) {
-        progressNumber.innerHTML = tasks[i]['progress'].length;
-    }
 }
 
 function showNumberOfTasksToDo() {
     let progressNumber = document.getElementById('toDo');
     progressNumber.innerHTML = '';
-    for (let i = 0; i < tasks.length; i++) {
-        progressNumber.innerHTML = tasks[i]['progress'].length;
-    }
 }
 
 function showNumberOfTasksDone() {
     let progressNumber = document.getElementById('done');
     progressNumber.innerHTML = '';
-    for (let i = 0; i < tasks.length; i++) {
-        progressNumber.innerHTML = tasks[i]['progress'].length;
-    }
 }
